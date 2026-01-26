17c02c35ed920211d1ab89d65faea5b84bc42724
jzking2019 <57752743+jzking2019@users.noreply.github.com>
2026-01-26 18:42:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -803,7 +803,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setTimeout(syncFooterToMobileMenu, 0);
 
   initBlogSearchAndPagination();
-  //initTurnstileGate(); // Turnstile 驗證
+  initTurnstileGate(); // Turnstile 驗證
   initTimelineCollapse(); // 時間節點摺疊
   initImageViewer(); // 圖片點擊放大
   init404Search(); // 404搜索
@@ -814,6 +814,7 @@ document.addEventListener("DOMContentLoaded", () => {
   loadVideo();
 
 });
+
 
 
 
