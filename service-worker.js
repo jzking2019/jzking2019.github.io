e2f9cb11cc7ab23c22701010d6162de8758eb2ad
jzking2019 <57752743+jzking2019@users.noreply.github.com>
2026-01-27 05:30:14 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('v2').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/?source=pwa', // 👈 確保 start_url 被快取
-                '/index.html',
-                '/manifest.json'
-            ]);
-        })
-    );
+const CACHE_NAME = "v3";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ])
+    )
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
